de623f3297e628d6792c5fadc2e07192fd1a80fa
EchoCrow <5488190+echocrow@users.noreply.github.com>
2025-12-08 21:25:40 +0100

    add 2025-08

---
+++ src/events/2025/08/1.ts
@@ -0,0 +1,66 @@
+import {sortNums} from '#lib/array.js'
+import io from '#lib/io.js'
+import {product} from '#lib/iterable.js'
+import {enqueue} from '#lib/queue.js'
+import {unionInto} from '#lib/set.js'
+import vec3, {type Vec3} from '#lib/vec3.js'
+
+const PAIRS = Number((await io.readCfgLine('__pairs')) ?? 1000)
+const TOP_CIRCUITS = 3
+
+class Box {
+  circuit: Set<Box> | undefined
+  constructor(readonly vec: Vec3) {}
+}
+
+const boxes: Box[] = []
+for await (const line of io.readLines()) boxes.push(new Box(vec3.parse(line)))
+
+class Pair {
+  readonly dist: number
+  constructor(
+    readonly a: Box,
+    readonly b: Box,
+  ) {
+    this.dist = a.vec.subtract(b.vec).len
+  }
+}
+
+const pairs: Pair[] = []
+for (let i = 0; i < boxes.length; i++) {
+  for (let j = i + 1; j < boxes.length; j++) {
+    const pair = new Pair(boxes[i]!, boxes[j]!)
+    enqueue(pairs, (a, b) => b.dist - a.dist, pair)
+  }
+}
+
+let remainingPairs = PAIRS
+while (remainingPairs > 0) {
+  const pair = pairs.pop()
+  if (!pair) break
+  const {a, b} = pair
+  if (!a.circuit || !b.circuit) {
+    const circuit = a.circuit ?? b.circuit ?? new Set()
+    circuit.add(a)
+    circuit.add(b)
+    a.circuit = b.circuit = circuit
+  } else if (a.circuit !== b.circuit) {
+    unionInto(a.circuit, b.circuit)
+    for (const box of b.circuit) box.circuit = a.circuit
+    b.circuit = a.circuit
+  }
+  remainingPairs--
+}
+
+const circuits = new Set(boxes.map((b) => b.circuit).filter((c) => !!c))
+
+const circuitLens = sortNums(
+  circuits
+    .values()
+    .map((c) => c.size)
+    .toArray(),
+  true,
+)
+
+const result = product(circuitLens.slice(0, TOP_CIRCUITS))
+io.write(result)
---
+++ src/events/2025/08/2.ts
@@ -0,0 +1,50 @@
+import io from '#lib/io.js'
+import {enqueue} from '#lib/queue.js'
+import {unionInto} from '#lib/set.js'
+import vec3, {type Vec3} from '#lib/vec3.js'
+
+class Box {
+  circuit: Set<Box> | undefined
+  constructor(readonly vec: Vec3) {}
+}
+
+const boxes: Box[] = []
+for await (const line of io.readLines()) boxes.push(new Box(vec3.parse(line)))
+
+class Pair {
+  readonly dist: number
+  constructor(
+    readonly a: Box,
+    readonly b: Box,
+  ) {
+    this.dist = a.vec.subtract(b.vec).len2
+  }
+}
+
+const pairs: Pair[] = []
+for (let i = 0; i < boxes.length; i++) {
+  for (let j = i + 1; j < boxes.length; j++) {
+    const pair = new Pair(boxes[i]!, boxes[j]!)
+    enqueue(pairs, (a, b) => b.dist - a.dist, pair)
+  }
+}
+
+let pair: Pair | undefined
+while ((boxes[0]?.circuit?.size ?? 0) < boxes.length) {
+  pair = pairs.pop()
+  if (!pair) break
+  const {a, b} = pair
+  if (!a.circuit || !b.circuit) {
+    const circuit = a.circuit ?? b.circuit ?? new Set()
+    circuit.add(a)
+    circuit.add(b)
+    a.circuit = b.circuit = circuit
+  } else if (a.circuit !== b.circuit) {
+    unionInto(a.circuit, b.circuit)
+    for (const box of b.circuit) box.circuit = a.circuit
+    b.circuit = a.circuit
+  }
+}
+
+const result = (pair?.a.vec[0] ?? 0) * (pair?.b.vec[0] ?? 0)
+io.write(result)
